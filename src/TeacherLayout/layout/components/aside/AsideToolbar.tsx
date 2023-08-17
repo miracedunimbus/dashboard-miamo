5cc36b11f7b8615bab8555e92f2db6a14e4ebf7c
Sait Ramazan <saitramazangul@gmail.com>
2023-08-18 01:15:50 +0300

    MAM-2 Static layout kontrolleri yapıldı

---
+++ src/TeacherLayout/layout/components/aside/AsideToolbar.tsx
@@ -0,0 +1,56 @@
+import {useAuth} from '../../../../app/modules/auth'
+import {KTIcon, toAbsoluteUrl} from '../../../../_metronic/helpers'
+import {HeaderUserMenu, Search} from '../../../../_metronic/partials'
+
+/* eslint-disable jsx-a11y/anchor-is-valid */
+const AsideToolbar = () => {
+  const {currentUser} = useAuth()
+
+  return (
+    <>
+      <div className='aside-user d-flex align-items-sm-center justify-content-center py-5'>
+        <div className='symbol symbol-50px'>
+          <img src={toAbsoluteUrl('/media/avatars/300-1.jpg')} alt='' />
+        </div>
+
+        <div className='aside-user-info flex-row-fluid flex-wrap ms-5'>
+          <div className='d-flex'>
+            <div className='flex-grow-1 me-2'>
+              <a href='#' className='text-white text-hover-primary fs-6 fw-bold'>
+                {currentUser?.first_name} {currentUser?.last_name}
+              </a>
+
+              <span className='text-gray-600 fw-bold d-block fs-8 mb-1'>Öğretmen</span>
+
+              <div className='d-flex align-items-center text-success fs-9'>
+                <span className='bullet bullet-dot bg-success me-1'></span>online
+              </div>
+            </div>
+
+            <div className='me-n2'>
+              <a
+                href='#'
+                className='btn btn-icon btn-sm btn-active-color-primary mt-n2'
+                data-kt-menu-trigger='click'
+                data-kt-menu-placement='bottom-start'
+                data-kt-menu-overflow='false'
+              >
+                <KTIcon iconName='setting-2' className='text-muted fs-1' />
+              </a>
+
+              <HeaderUserMenu />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/*begin::Aside search*/}
+      {/* <div className='aside-search py-5'>
+        <Search />
+      </div> */}
+      {/*end::Aside search*/}
+    </>
+  )
+}
+
+export {AsideToolbar}
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,39 @@
 import {Suspense} from 'react'
 import {Outlet} from 'react-router-dom'
 import {I18nProvider} from '../_metronic/i18n/i18nProvider'
-import {LayoutProvider, LayoutSplashScreen} from '../_metronic/layout/core'
-import {MasterInit} from '../_metronic/layout/MasterInit'
+import {LayoutProvider, LayoutSplashScreen} from '../TeacherLayout/layout/core'
+import {MasterInit} from '../TeacherLayout/layout/MasterInit'
 import {AuthInit} from './modules/auth'
 import {ThemeModeProvider} from '../_metronic/partials'
 
 const App = () => {
+  const user = "teacher";
+  const role = "manager"
   return (
     <Suspense fallback={<LayoutSplashScreen />}>
       <I18nProvider>
-        <LayoutProvider>
-          <ThemeModeProvider>
-            <AuthInit>
-              <Outlet />
-              <MasterInit />
-            </AuthInit>
-          </ThemeModeProvider>
-        </LayoutProvider>
+        {
+          (user === role.toString())
+          && 
+          (
+            <LayoutProvider>
+              <ThemeModeProvider>
+                <AuthInit>
+                  <Outlet />
+                  <MasterInit />
+                </AuthInit>
+              </ThemeModeProvider>
+            </LayoutProvider>
+          )
+        }
+
+        {
+          !(user === role.toString())
+          && 
+          (
+            <p className='w-75 mx-auto mt-5 bg-dark text-white p-5 text-center'> Kullanıcı rolleri eşit olmadığı için şu an bu alanı görüyorsun. App.tsx içinde static bir kontrol var onu eşitlerse bu hata düzelir.  </p>
+          )
+        }
       </I18nProvider>
     </Suspense>
   )
