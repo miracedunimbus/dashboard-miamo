be9ce922a462227a38be2f88b946d45e57d3a7a1
Miray Kaptan <141050374+miracedunimbus@users.noreply.github.com>
2023-08-23 17:18:23 +0300

    Report 3 changed

--- src/app/modules/profile/components/Documents.tsx
+++ src/app/modules/profile/components/Documents.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {KTIcon} from '../../../../_metronic/helpers'
 import {Card4} from '../../../../_metronic/partials/content/cards/Card4'
 import ChartWidget9 from '../../../../_metronic/partials/widgets/charts/ChartsWidget9'
+import ChartWidget10 from '../../../../_metronic/partials/widgets/charts/ChartsWidget10'
 
 
 export function Documents() {
@@ -11,14 +12,11 @@ export function Documents() {
       <div className='d-flex flex-wrap flex-stack mb-6'>
         <h3 className='fw-bolder my-2'>
           Rapor 3
-          <span className='fs-6 text-gray-400 fw-bold ms-1'>Miray hanımdan alınacak veriler</span>
         </h3>
       </div>
-      <a href="https://preview.keenthemes.com/metronic8/demo39/index.html?mode=light">
-        1 yıllık geçmiş eğitimlerin toplam oranları hepsi tüm bir yılı kapsayacak şekilde olacak
-      </a>
-      <br />
+
 <ChartWidget9/>
+{/* <ChartWidget10/> */}
     </>
   )
 }
