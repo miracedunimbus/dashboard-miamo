e66302a585d9b96157811f29a3054886e93c6842
Miray Kaptan <141050374+miracedunimbus@users.noreply.github.com>
2023-08-23 17:11:28 +0300

    MixedWidget9 description property added and changed description.

--- src/app/modules/profile/components/Campaigns.tsx
+++ src/app/modules/profile/components/Campaigns.tsx
@@ -1,61 +1,65 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import {Card5} from '../../../../_metronic/partials/content/cards/Card5'
-import { MixedWidget9Copy } from '../../../../_metronic/partials/widgets/mixed/MixedWidget9-copy'
+import React from "react";
+import { Card5 } from "../../../../_metronic/partials/content/cards/Card5";
+import { MixedWidget9Copy } from "../../../../_metronic/partials/widgets/mixed/MixedWidget9-copy";
 
 export function Campaigns() {
-  const array = [1,2,3,4,5,6,7]
+  const array = [1, 2, 3, 4, 5, 6, 7];
 
   return (
     <>
-      <div className='d-flex flex-wrap flex-stack mb-6'>
-        <h3 className='fw-bolder my-2'>
-          Raporum
-        </h3>
+      <div className="d-flex flex-wrap flex-stack mb-6">
+        <h3 className="fw-bolder my-2">Raporum</h3>
       </div>
 
       <div className="d-flex">
-      <MixedWidget9Copy
-            className='card-xl-stretch mb-xl-2 me-auto'
-            chartColor='primary'
-            chartHeight='150px'
-          />
-          <MixedWidget9Copy
-            className='card-xl-stretch mb-xl-2 me-auto'
-            chartColor='primary'
-            chartHeight='150px'
-          />
-          <MixedWidget9Copy
-            className='card-xl-stretch mb-xl-2 me-auto'
-            chartColor='primary'
-            chartHeight='150px'
-          />
+        <MixedWidget9Copy
+          className="card-xl-stretch mb-xl-2 me-auto"
+          chartColor="primary"
+          chartHeight="150px"
+          description={"Pazartesi"}
+        />
+        <MixedWidget9Copy
+          className="card-xl-stretch mb-xl-2 me-auto"
+          chartColor="primary"
+          chartHeight="150px"
+          description={"Salı"}
+        />
+        <MixedWidget9Copy
+          className="card-xl-stretch mb-xl-2 me-auto"
+          chartColor="primary"
+          chartHeight="150px"
+          description={"Çarşamba"}
+        />
       </div>
-      <div className='d-flex'
-      >
-<MixedWidget9Copy
-            className='card-xl-stretch mb-xl-2 me-auto'
-            chartColor='primary'
-            chartHeight='150px'
-          />
-          <MixedWidget9Copy
-            className='card-xl-stretch mb-xl-2 me-auto'
-            chartColor='primary'
-            chartHeight='150px'
-          />
-          <MixedWidget9Copy
-            className='card-xl-stretch mb-xl-2 me-auto'
-            chartColor='primary'
-            chartHeight='150px'
-          />
+      <div className="d-flex">
+        <MixedWidget9Copy
+          className="card-xl-stretch mb-xl-2 me-auto"
+          chartColor="primary"
+          chartHeight="150px"
+          description={"Perşembe"}
+        />
+        <MixedWidget9Copy
+          className="card-xl-stretch mb-xl-2 me-auto"
+          chartColor="primary"
+          chartHeight="150px"
+          description={"Cuma"}
+        />
+        <MixedWidget9Copy
+          className="card-xl-stretch mb-xl-2 me-auto"
+          chartColor="primary"
+          chartHeight="150px"
+          description={"Cumartesi"}
+        />
       </div>
-      <div className='d-flex'>
-      <MixedWidget9Copy
-            className='card-xl-stretch mb-xl-2 me-auto'
-            chartColor='primary'
-            chartHeight='150px'
-          />
+      <div className="d-flex">
+        <MixedWidget9Copy
+          className="card-xl-stretch mb-xl-2 me-auto"
+          chartColor="primary"
+          chartHeight="150px"
+          description={"Pazar"}
+        />
       </div>
     </>
-  )
+  );
 }
